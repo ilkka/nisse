834f7db99d3160c593fb896e76731c335cee0b52
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2023-03-06 15:55:20 +0200

    Remove compilers list from mix.exs
    Not needed in 1.7 any more.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,6 @@ defmodule Nisse.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
