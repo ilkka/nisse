a43da8b994bbc6f9172fbaa95e8e9d0f91467693
Ilkka Poutanen <ilkka@ilkka.dev>
2023-05-22 08:26:53 +0300

    UserSocket is not by default present in new builds

--- lib/nisse_web/endpoint.ex
+++ lib/nisse_web/endpoint.ex
@@ -10,11 +10,6 @@ defmodule NisseWeb.Endpoint do
     signing_salt: "YXxYCcME"
   ]
 
-  socket("/socket", NisseWeb.UserSocket,
-    websocket: true,
-    longpoll: false
-  )
-
   socket("/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]])
 
   # Serve at "/" the static files from "priv/static" directory.
